b67a57ef026a6ac617d5c7f986156881fbfc5a86
ustyui <45918907+ustyui@users.noreply.github.com>
2019-01-27 14:06:17 +0900

    auto change to multi channels, more simple codes

--- src/auto.py
+++ src/auto.py
@@ -13,6 +13,7 @@ import rospy
 from silva_beta.msg import Evans
 
 import os, threading
+import numpy as np
 
 import transformations as tform
 # import numpy as np
@@ -39,7 +40,7 @@ class poseblock():
 
         # global variables
         self._rel = [] # relative value
-        self._bias =[] # bias value for subposition
+        self._bias =[[],[],[],[]] # bias value for subposition
         self._payload = [] # final payload
         self._default = [] # origin default
 
@@ -52,35 +53,49 @@ class poseblock():
         self.pub = rospy.Publisher('/silva/joint_local/auto', Evans, queue_size=10)
 
         # subscribers
-        self.sub_int = rospy.Subscriber('/silva/auto_local/intention', Evans, self.intention_cb)
+        self.sub_ch0 = rospy.Subscriber('/silva/auto_local/ch0', Evans, self.ch0_cb)    # ch0 for blink message (headl)
+        self.sub_ch1 = rospy.Subscriber('/silva/auto_local/ch1', Evans, self.ch1_cb)    # ch1 for realsense (neck)
+        self.sub_ch2 = rospy.Subscriber('/silva/auto_local/ch2', Evans, self.ch2_cb)    # ch2 for respeaker (neck)
+        self.sub_ch3 = rospy.Subscriber('/silva/auto_local/ch3', Evans, self.ch3_cb)    # ch3 for rplidar (hip?)
+        
         self.sub_default = rospy.Subscriber('/silva/joint_local/default', Evans, self.default_cb)
         
         #init the default message
         tform.set_zeros(self._default)
-        tform.set_zeros(self._rel)
+        tform.set_zeros(self._rel)  # set zeros of rel
+        for i in range(len(self._bias)):
+            tform.set_zeros(self._bias[i]) # set zeros of bias
     
     
     ### callback functions ###
+    def decision_cb(self, msg):
+        return None
+    
+    ## multichannel callback ##
  
-    def intention_cb(self, msg):
-        # this callbacks /silva/auto_local/intention
-
-        self._intention = msg
-        
-        # cut method : from where
-        _cut = seq_of_jointname[msg.name]
-        
-        # get the payload
-        _payload = msg.payload
-        
-        # if the msgid = 1 then to relative
-        if msg.msgid == 1:
-            
-            # place payload to the cut place
-            for _idx in range (0, len(_payload)):
-                self._rel[_cut*5 + _idx] = _payload[_idx]
+    def ch0_cb(self, msg):  
+        _cut = seq_of_jointname[msg.name]        # cut method : from where
+        _payload = msg.payload                   # get the payload
+        for _idx in range (0, len(_payload)):
+            self._bias[0][_cut*5 + _idx] = _payload[_idx] # store payload in bias
                 
-               
+    def ch1_cb(self, msg):
+        _cut = seq_of_jointname[msg.name]        
+        _payload = msg.payload                  
+        for _idx in range (0, len(_payload)):
+            self._bias[1][_cut*5 + _idx] = _payload[_idx] 
+            
+    def ch2_cb(self, msg):
+        _cut = seq_of_jointname[msg.name]        
+        _payload = msg.payload                   
+        for _idx in range (0, len(_payload)):
+            self._bias[2][_cut*5 + _idx] = _payload[_idx] 
+            
+    def ch3_cb(self, msg):
+        _cut = seq_of_jointname[msg.name]        
+        _payload = msg.payload                   
+        for _idx in range (0, len(_payload)):
+            self._bias[3][_cut*5 + _idx] = _payload[_idx] 
 
     def default_cb(self, msg):
         # callback default
@@ -88,25 +103,14 @@ class poseblock():
         self._default = msg.payload
         
         
-
-    # set message from position change
-    # receive sequence and decide cut from where
-    # add the array to that sequence
+    ### merge bias ###
     def set_msg_from_pos(self):
-
+        
+        mult_ch = np.array(self._bias)
+        self._rel = mult_ch[0] + mult_ch[1] + mult_ch[2] +mult_ch[3]
         # add intentions to default
         self._payload = self._rel
-        
-    # make the message
-    def make_message(self):
-        
-        # make message
-        self._pub_msg.header.stamp = rospy.Time.now()
-        self._pub_msg.seq = 4
-        self._pub_msg.name = 'auto'
-        self._pub_msg.msgid = 0
-        self._pub_msg.payload = self._payload
-
+    
         
     # TODO: make the filter
     def joint_filter(self, mask):
@@ -127,7 +131,7 @@ class poseblock():
             self.set_msg_from_pos()
             
             # make the message
-            self.make_message()
+            tform.make_message(self._pub_msg, 4, 'auto', 0, self._payload)
             
             # publish the message
             self.pub.publish(self._pub_msg)
